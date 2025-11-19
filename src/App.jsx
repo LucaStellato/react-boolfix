bef39bea700eacfb54fc25b430dcad65d4e04cfc
LucaStellato <lucastellato435@gmail.com>
2025-11-19 13:02:32 +0100

    import axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const endpointKey = import.meta.env.VITE_API_KEY;
   const [film, setFilm] = useState([]);
   const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${endpointKey}`
+  useEffect(fetchFilm, [])
   function fetchFilm() {
     axios.get(endpoint).then(response => {
       setFilm(response.data)
