b2ccbf08caec72df6ed3b51cf0dd7a0339830ce2
LucaStellato <lucastellato435@gmail.com>
2025-11-19 13:00:50 +0100

    create function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ function App() {
   const endpointKey = import.meta.env.VITE_API_KEY;
   const [film, setFilm] = useState([]);
   const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${endpointKey}`
+  function fetchFilm() {
+    axios.get(endpoint).then(response => {
+      setFilm(response.data)
+    })
+  }
   return (
     <>
 
